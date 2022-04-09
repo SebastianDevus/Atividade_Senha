392bb23da509e90674021f17ea16eb4295ecaa99
Sebastian-Devus <sebastiandevus@gmail.com>
2022-04-08 21:58:18 -0300

    Teste de senha simples concluido

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string senhaDigitada;
+
+// Espacamento
+Console.WriteLine();
+
+// Solicitando e armazenando uma senha digitada pelo usuário
+Console.Write("Olá usuário. Digite sua senha: ");
+senhaDigitada = Console.ReadLine()!;
+
+// Testando se a senha é igual a correta (1234abcd)
+if (senhaDigitada == "1234abcd")
+{
+    // Mensagem de sucesso, em cor verde.
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Acesso autorizado");
+}
+else
+{
+    // Mensagem de erro, em cor vermelha, com som
+    Console.Beep();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Acesso negado");
+}
+
+// Resetando a cor
+Console.ResetColor();
+
+// Espacamento
+Console.WriteLine();
